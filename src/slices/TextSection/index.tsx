8df2e4d32548d1191ea7523fde285f7c06e2955d
Dharmik Jagodana <dharmikjagodana007@gmail.com>
2024-05-02 23:43:02 +0530

    rich content max width and in center

--- src/slices/TextSection/index.tsx
+++ src/slices/TextSection/index.tsx
@@ -13,19 +13,24 @@ export type TextSectionProps = SliceComponentProps<Content.TextSectionSlice>
  */
 const TextSection = ({ slice }: TextSectionProps): JSX.Element => {
   return (
-    <Section withVerticalPadding>
+    <Section withVerticalPadding className="w-full">
       <section
         data-slice-type={slice.slice_type}
         data-slice-variation={slice.variation}
+        className="flex flex-col items-center content-center w-full"
       >
-        <div>
-          <h1 className="text-4xl">
-            <PrismicRichText field={slice.primary.heading} />
-          </h1>
-          <p className='text-base font-semibold text-gray-400'>{slice.primary.description}</p>
-        </div>
-        <div className='my-4 sm:my-8'>
-          <PrismicRichText field={slice.primary.content} />
+        <div className="flex flex-col max-w-2xl">
+          <div className="flex flex-col justify-start">
+            <h1 className="text-4xl">
+              <PrismicRichText field={slice.primary.heading} />
+            </h1>
+            <p className="text-base font-semibold text-gray-400">
+              {slice.primary.description}
+            </p>
+          </div>
+          <div className="my-4 sm:my-8">
+            <PrismicRichText field={slice.primary.content} />
+          </div>
         </div>
       </section>
     </Section>
